d422c02b9f7550bced429ed1e9b47cb8619f26e3
Martin <mdk@amescon.com>
2014-04-25 09:19:07 +0200

    changed destination of output file in build.bat

--- vs/build.bat
+++ vs/build.bat
@@ -1,11 +1,11 @@
 @echo ****************
 @echo *** Building ***
 @echo ****************
-@arm-linux-gnueabihf-g++.exe ..\*.c -o tty-send
+@arm-linux-gnueabihf-g++.exe ..\*.c -o ..\tty-send
 
 @IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
 
 @echo *****************
 @echo *** Deploying ***
 @echo *****************
-@scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ./tty-send pi@192.168.2.111:~/
+@scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ./../tty-send pi@192.168.2.111:~/
