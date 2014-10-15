acf8e49ebd3413122ce46890ed3af0f120a48d5d
Martin <mdk@amescon.com>
2014-10-15 18:22:26 +0200

    updated build.bat to link against library for timing functions

--- vs/build.bat
+++ vs/build.bat
@@ -1,6 +1,6 @@
 @echo ****************
 @echo *** Building ***
 @echo ****************
-@C:\SysGCC\Raspberry\bin\arm-linux-gnueabihf-g++.exe ..\*.c -lpthread -o ..\tty-send
+@C:\SysGCC\Raspberry\bin\arm-linux-gnueabihf-g++.exe ..\*.c -lpthread -lrt -o ..\tty-send
 
 @IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
